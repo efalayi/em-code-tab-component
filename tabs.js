848b985e4583aa1addc9019d7819b6e55153b9e7
Esther Falayi <esther.falayi@gmail.com>
2025-03-17 09:15:44 +0000

    (feat): tab component
    - update style.css
    - add title to tab content divs

--- tabs.js
+++ tabs.js
@@ -14,23 +14,21 @@ class TabsComponent extends HTMLElement {
     const tabNavItems = this.querySelectorAll('[slot="tab"]');
     const tabContents = this.querySelectorAll('[slot="content"]');
 
-    console.log('tabNavItems: ', tabNavItems);
+    tabNavItems.forEach(navItem => {
+      navItem.addEventListener('click', function () {
+        const tabKey = this.getAttribute("data-tab-key");
 
-    // tabNavItems.forEach(navItem => {
-    //   navItem.addEventListener('click', function () {
-    //     const tabKey = this.getAttribute("data-tab-key");
+        tabNavItems.forEach(navItem => navItem.classList.remove("active"));
+        tabContents.forEach(content => content.classList.remove("active"));
 
-    //     tabNavItems.forEach(navItem => navItem.classList.remove("active"));
-    //     tabContents.forEach(content => content.classList.remove("active"));
+        this.classList.add("active");
+        document.querySelector(`.tab-content[data-content-key='${tabKey}']`).classList.add("active");
+      });
+    });
 
-    //     this.classList.add("active");
-    //     document.querySelector(`.tab-content[data-content-key='${tabKey}']`).classList.add("active");
-    //   });
-    // });
-
-    // // Set the first tab as active by default
+    // Set the first tab as active by default
     tabNavItems[0].classList.add("active");
-    // tabContents[0].classList.add("active");
+    tabContents[0].classList.add("active");
   }
 }
 
