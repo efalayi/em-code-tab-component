5fc2b9a0096092c7dd597698fd01925400e6f6b0
Esther Falayi <esther.falayi@gmail.com>
2025-03-17 09:04:00 +0000

    (feat); tab component
    - create a render function for TabComponent
    - add eventListener function to handle tab-nav-item click
    - add defer to script tag

--- tabs.js
+++ tabs.js
@@ -5,10 +5,35 @@ class TabsComponent extends HTMLElement {
   constructor() {
     super();
   }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const tabNavItems = this.querySelectorAll('[slot="tab"]');
+    const tabContents = this.querySelectorAll('[slot="content"]');
+
+    console.log('tabNavItems: ', tabNavItems);
+
+    // tabNavItems.forEach(navItem => {
+    //   navItem.addEventListener('click', function () {
+    //     const tabKey = this.getAttribute("data-tab-key");
+
+    //     tabNavItems.forEach(navItem => navItem.classList.remove("active"));
+    //     tabContents.forEach(content => content.classList.remove("active"));
+
+    //     this.classList.add("active");
+    //     document.querySelector(`.tab-content[data-content-key='${tabKey}']`).classList.add("active");
+    //   });
+    // });
+
+    // // Set the first tab as active by default
+    tabNavItems[0].classList.add("active");
+    // tabContents[0].classList.add("active");
+  }
 }
 
-(function () {
-  customElements.define("tabs-component", TabsComponent);
+customElements.define("tabs-component", TabsComponent);
 
-  console.log('log from tabs.js');
-})();
+console.log('log from tabs.js');
